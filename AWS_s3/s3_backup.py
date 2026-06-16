05433776361adf298fa2e117657750b8c089f8cb
SaiSamarth59 <saisamarthbukka1431@gmail.com>
2026-06-16 21:45:58 +0530

    added all file

--- AWS_s3/s3_backup.py
+++ AWS_s3/s3_backup.py
@@ -4,3 +4,12 @@ s3=boto3.resource("s3")
 def show_buckets(s3):
     for bucket in s3.buckets.all():
         print(bucket.name)
+def create_bucket(s3):
+    s3.create_bucket(Bucket="python-for-devops-59",
+                     CreateBucketConfiguration={
+                     'LocationConstraint': 'us-east-2',    
+                     },)
+    print("Bucket created successfully")
+
+create_bucket(s3)
+show_buckets(s3)
